709c6b57a0bbdecfce2b99868a98b97bbce08363
DinoLaudisio <dinolaudisio1@gmail.com>
2023-10-19 16:07:45 +0200

    Initial Commit

---
+++ DinoTest/DinoTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DinoTestApp.swift
+//  DinoTest
+//
+//  Created by Dino Laudisio on 19/10/23.
+//
+
+import SwiftUI
+
+@main
+struct DinoTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
